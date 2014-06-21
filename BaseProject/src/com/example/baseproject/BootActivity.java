286f4d81ee4b5142e5b1fb81a0692fdf3bb2fd41
wanglg <670500876@qq.com>
2014-06-21 17:22:57 +0800

    first created

---
+++ BaseProject/src/com/example/baseproject/BootActivity.java
@@ -0,0 +1,57 @@
+package com.example.baseproject;
+
+import com.example.bean.Device;
+
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.view.Menu;
+
+public class BootActivity extends Activity {
+	public final String PACKAGE_NAME="com.example.baseproject";
+	public final String VERSION_KEY="version";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(android.R.style.Theme_Translucent_NoTitleBar);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boot);
+    }
+
+    public boolean isFirstInstall(){
+    	PackageInfo info=null;
+		try {
+			info = getPackageManager().getPackageInfo(PACKAGE_NAME, 0);
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	int currentVersion = info.versionCode;
+    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    	int lastVersion = prefs.getInt(VERSION_KEY, 0);
+    	if (currentVersion > lastVersion) {
+    	     //如果当前版本大于上次版本，该版本属于第一次启动
+    	  
+    	     //将当前版本写入preference中，则下次启动的时候，据此判断，不再为首次启动
+    	     prefs.edit().putInt(VERSION_KEY,currentVersion).commit();
+    	     return true;
+    	}else{
+    		 return false;
+    	}
+    }
+    
+    public void initDevice(){
+    	Device device=Device.getInstance();
+    	device.init(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.boot, menu);
+        return true;
+    }
+    
+}
---
+++ BaseProject/src/com/example/bean/Device.java
@@ -0,0 +1,67 @@
+package com.example.bean;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.view.Display;
+
+import com.example.util.LogUtil;
+
+public class Device {
+	private final String TAG="wang";
+	private static Device device=new Device();
+	private Device(){
+		
+	}
+	public static Device getInstance(){
+		return device;
+		
+	}
+	public void init(Activity context){
+		
+		Display display =context.getWindowManager().getDefaultDisplay();
+		int width = display.getWidth();
+		int height = display.getHeight();
+		
+		WifiManager wifi = (WifiManager)context. getSystemService (Context.WIFI_SERVICE);
+        WifiInfo info = wifi.getConnectionInfo();
+        setMAC_address(info.getMacAddress()) ;
+        setSystemVersion(android.os.Build.VERSION.SDK_INT);
+		setWidth(width);
+		setHeight(height);
+		LogUtil.i(TAG, "width-->"+width+"  height---> "+height+"" +
+				"MAC_address-->"+MAC_address+"  SystemVersion--->"+getSystemVersion());
+	}
+	int width;
+	int height;
+	String MAC_address;
+	int systemVersion;
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public String getMAC_address() {
+		return MAC_address;
+	}
+	public void setMAC_address(String mAC_address) {
+		MAC_address = mAC_address;
+	}
+	public int getSystemVersion() {
+		return systemVersion;
+	}
+	public void setSystemVersion(int systemVersion) {
+		this.systemVersion = systemVersion;
+	}
+
+	
+
+}
