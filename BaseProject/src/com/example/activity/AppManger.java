9cad297256e38a59f4b17e8224e6638c2d687bcb
wanglg <670500876@qq.com>
2014-09-02 18:06:54 +0800

    appManger

---
+++ BaseProject/src/com/example/activity/AppManger.java
@@ -0,0 +1,18 @@
+package com.example.activity;
+
+import android.app.Application;
+
+public class AppManger extends Application {
+	public static AppManger instant;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		instant=this;
+	}
+	
+	public static AppManger getInstant() {
+		return instant;
+	}
+
+}
